e2b61318c6cc4e1fc47885d64fad8f22bda34a86
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 23:34:08 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -55,6 +55,7 @@ export function DepartmentPage() {
 
   const departmentLogoSrc = new URL(
     `../../../public/images/${getLogoForDepartment(department.name)}`,
+    import.meta.url
   const allCharters = getChartersByDepartment(deptId);
   const charters = allCharters.filter(
     (c) =>
