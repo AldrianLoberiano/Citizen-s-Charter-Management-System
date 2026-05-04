213eef9fd4f367e258a66885ea6ad56482c64b4e
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 23:33:51 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -34,6 +34,7 @@ const departmentLogos: Record<string, string> = {
   "Human Resources": "hrmo-removebg-preview.png",
   "Civil Registry": "lydo.png",
   "Assessor's Office": "assesors_office-removebg-preview.png",
+  "Agriculture": "AgriOffice-removebg-preview.png",
 
 export function DepartmentPage() {
   const { id } = useParams<{ id: string }>();
