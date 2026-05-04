256584ae3f4c013a3f14aa3f8399f04edb8a6c82
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 23:37:36 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -21,7 +21,6 @@ import {
   formatDate,
 } from "../../store/data";
 
-  "Social Welfare and Development": "osca.png",
   "Engineering and Public Works": "mpdc-removebg-preview.png",
   "Health Services": "mho-removebg-preview.png",
   "Tourism and Culture": "mto-removebg-preview.png",
