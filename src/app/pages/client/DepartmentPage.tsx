ffad9011c3117279218705c04192cd2771197452
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 22:47:38 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -64,6 +64,7 @@ export function DepartmentPage() {
                 alt="Calauan City Seal"
                 className="h-full w-full object-contain"
               />
+            </div>
             <div>
               <h1 className="text-2xl sm:text-3xl font-semibold leading-tight text-slate-950 normal-case">
                 {department.name}
