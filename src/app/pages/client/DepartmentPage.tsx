aafacf0fb4b9a42e3af4a6e4579208be752ecb0b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 22:41:57 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -62,7 +62,6 @@ export function DepartmentPage() {
           </nav>
 
           {/* Department Info */}
-            </div>
             <div>
               <h1 className="leading-tight text-slate-950">{department.name}</h1>
               <p className="mt-2 max-w-2xl text-sm leading-relaxed text-slate-600">
