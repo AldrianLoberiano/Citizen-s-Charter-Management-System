d1abbccc7a2f12b9a9fcaf4eb1f0179605c60995
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 22:41:40 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -62,7 +62,6 @@ export function DepartmentPage() {
           </nav>
 
           {/* Department Info */}
-            <div className="flex h-14 w-14 flex-shrink-0 items-center justify-center bg-transparent p-0">
                 src={logoSrc}
                 alt={`${department.name} logo`}
                 className="h-full w-full object-contain"
