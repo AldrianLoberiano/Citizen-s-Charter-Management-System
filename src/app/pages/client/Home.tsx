197b295c3e7d2318ffbc38e9b19620a7b3e55197
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 23:53:34 +0800

    Update Home.tsx

--- src/app/pages/client/Home.tsx
+++ src/app/pages/client/Home.tsx
@@ -116,7 +116,6 @@ export function Home() {
               <img
                 src={calauanLogo2Src}
                 alt="Calauan logo"
-                className="h-14 w-14 object-contain"
                 loading="lazy"
                 decoding="async"
               />
