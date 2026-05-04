8ff83abfd614e1f76f05d88b1b2a68898d0f721d
AldrianLoberiano <loberianorian@gmail.com>
2026-05-04 22:47:11 +0800

    Update DepartmentPage.tsx

--- src/app/pages/client/DepartmentPage.tsx
+++ src/app/pages/client/DepartmentPage.tsx
@@ -30,7 +30,6 @@ export function DepartmentPage() {
   const deptId = parseInt(id || "0");
   const department = getDepartmentById(deptId);
 
-
   // Redirect if department not found
   if (!department) {
     return <Navigate to="/" replace />;
