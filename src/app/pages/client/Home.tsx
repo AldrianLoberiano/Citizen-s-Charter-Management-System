29ecd8ef739d2ca1624a8875645d11d68c52ca5b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-06 23:30:03 +0800

    Update Home.tsx

--- src/app/pages/client/Home.tsx
+++ src/app/pages/client/Home.tsx
@@ -223,7 +223,6 @@ export function Home() {
                   to={`/department/${dept.id}`}
                   className="group flex flex-col rounded-2xl border border-slate-200 p-6 transition-all hover:border-slate-300 hover:shadow-sm"
                   style={{
-                    backgroundImage: `url(${lightVioletBgSrc})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                   }}
