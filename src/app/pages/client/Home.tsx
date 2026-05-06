334570e0188919e209ed4479a1a91904f66198fc
AldrianLoberiano <loberianorian@gmail.com>
2026-05-06 23:39:25 +0800

    Update Home.tsx

--- src/app/pages/client/Home.tsx
+++ src/app/pages/client/Home.tsx
@@ -235,6 +235,7 @@ export function Home() {
 
                   {/* Content */}
                   <div className="flex-1">
+                    <h3 className="mb-2 text-lg font-semibold leading-tight text-white transition-colors group-hover:text-white">
                       {dept.name}
                     </h3>
                     <p className="line-clamp-3 text-sm leading-relaxed text-slate-600">
