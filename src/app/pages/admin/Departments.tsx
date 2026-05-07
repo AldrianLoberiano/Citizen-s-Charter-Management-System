abe1fb02604a61c818d76f0e90f79aabf5df0de0
AldrianLoberiano <loberianorian@gmail.com>
2026-05-07 14:24:16 +0800

    Update Departments.tsx

--- src/app/pages/admin/Departments.tsx
+++ src/app/pages/admin/Departments.tsx
@@ -199,7 +199,6 @@ export function Departments() {
           <table className="w-full">
             <thead>
               <tr className="border-b border-slate-200 bg-slate-50">
-                  ID
                 </th>
                 <th className="text-left px-5 py-3 text-slate-500 text-xs uppercase tracking-wide">
                   Department Name
