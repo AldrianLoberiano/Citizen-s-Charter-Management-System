2fd5eccb184e285959469c6f46e5df161c317715
AldrianLoberiano <loberianorian@gmail.com>
2026-05-07 08:28:25 +0800

    Update ClientLayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -73,7 +73,6 @@ export function ClientLayout() {
               </p>
             </div>
 
-              <ul className="space-y-2 text-sm">
                 <li>
                   <Link to="/" className="text-slate-500 transition-colors hover:text-slate-900">
                     Home
