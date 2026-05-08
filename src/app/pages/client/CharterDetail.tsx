2e0bcabea5c26552adb3d3ca8740af5bb62cc6bb
AldrianLoberiano <loberianorian@gmail.com>
2026-05-08 18:33:37 +0800

    Update CharterDetail.tsx

--- src/app/pages/client/CharterDetail.tsx
+++ src/app/pages/client/CharterDetail.tsx
@@ -548,7 +548,6 @@ export function CharterDetail() {
               </dl>
             </div>
 
-            <div className="bg-white rounded-xl border border-slate-200 p-5 space-y-2">
               {department && (
                 <Link
                   to={`/department/${department.id}`}
