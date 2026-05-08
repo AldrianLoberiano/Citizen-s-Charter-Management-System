bb7615f3985ca73d5ae7b9dba0c9628116750218
AldrianLoberiano <loberianorian@gmail.com>
2026-05-08 18:33:39 +0800

    Update CharterDetail.tsx

--- src/app/pages/client/CharterDetail.tsx
+++ src/app/pages/client/CharterDetail.tsx
@@ -548,7 +548,6 @@ export function CharterDetail() {
               </dl>
             </div>
 
-              {department && (
                 <Link
                   to={`/department/${department.id}`}
                   className="w-full flex items-center gap-2 py-2.5 px-3 text-slate-600 hover:bg-slate-50 rounded-lg transition-colors text-sm"
