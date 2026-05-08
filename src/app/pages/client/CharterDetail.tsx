6a068abe78fd7cf79c355c4027824bc1be5fe441
AldrianLoberiano <loberianorian@gmail.com>
2026-05-08 18:34:19 +0800

    Update CharterDetail.tsx

--- src/app/pages/client/CharterDetail.tsx
+++ src/app/pages/client/CharterDetail.tsx
@@ -548,7 +548,6 @@ export function CharterDetail() {
               </dl>
             </div>
 
-              <a
                 href="#"
                 onClick={(e) => {
                   e.preventDefault();
