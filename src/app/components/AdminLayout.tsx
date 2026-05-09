0057379e1fe2e5fbadc071a559e777e15e0cc728
AldrianLoberiano <loberianorian@gmail.com>
2026-05-10 07:59:50 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -84,7 +84,6 @@ export function AdminLayout() {
     navigate("/admin/login");
   };
 
-      const blob = await api.downloadBackup();
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
