e2b811ae4de7d26166c02ce3538c6bfe13afc8fa
AldrianLoberiano <loberianorian@gmail.com>
2026-05-11 22:45:30 +0800

    Update Feedback.tsx

--- src/app/pages/admin/Feedback.tsx
+++ src/app/pages/admin/Feedback.tsx
@@ -575,6 +575,7 @@ export function Feedback() {
                       >
                         View
                       </button>
+                    </td>
                   </tr>
                 );
               })}
