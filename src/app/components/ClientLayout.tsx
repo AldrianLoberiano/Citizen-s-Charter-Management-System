d612c44e711ab1eec2a52642629b30887fcf60c0
AldrianLoberiano <loberianorian@gmail.com>
2026-05-11 21:55:50 +0800

    Update ClientLayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -167,6 +167,7 @@ export function ClientLayout() {
             </div>
 
             {/* Contact */}
+            <div className="md:text-right">
                   <MapPin className="mt-0.5 h-4 w-4 flex-shrink-0 text-slate-400" />
                   <span>Municipal Hall, Poblacion, Municipality</span>
                 </li>
