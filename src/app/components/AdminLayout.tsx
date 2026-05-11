0c11cecf3b21bfa76ff6af6bdc1c7da71e7b450c
AldrianLoberiano <loberianorian@gmail.com>
2026-05-11 23:01:46 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -114,6 +114,7 @@ export function AdminLayout() {
     };
 
     document.addEventListener("mousedown", handleClick);
+    document.addEventListener("keydown", handleKey);
   const handleLogout = () => {
     logout();
     navigate("/admin/login");
