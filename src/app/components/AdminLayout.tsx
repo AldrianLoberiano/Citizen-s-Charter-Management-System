7926f752a33ec531fdd456ae981a4afda8e1763b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-11 22:57:57 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -103,6 +103,7 @@ export function AdminLayout() {
       if (!mobileNavRef.current) return;
       if (!mobileNavRef.current.contains(event.target as Node)) {
         setMobileNavOpen(false);
+      }
   const handleLogout = () => {
     logout();
     navigate("/admin/login");
