2f8538fca02ce8569f610134826f9511d515fe94
AldrianLoberiano <loberianorian@gmail.com>
2026-05-11 22:49:39 +0800

    Update Feedback.tsx

--- src/app/pages/admin/Feedback.tsx
+++ src/app/pages/admin/Feedback.tsx
@@ -634,6 +634,7 @@ export function Feedback() {
                 <p className="text-xs uppercase tracking-wide text-slate-400">Rating</p>
                 <p className="text-slate-900">{selectedFeedback.rating}</p>
               </div>
+              <div>
       <Modal
         isOpen={isQrOpen}
         onClose={() => setIsQrOpen(false)}
