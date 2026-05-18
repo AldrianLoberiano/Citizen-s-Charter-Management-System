1fdccdd5cfcfd0873156842fb5b30aec45e927bb
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:07:26 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,6 @@ app.post("/api/uploads/charters", upload.single("file"), (req, res) => {
 });
 
 
-
   const baseName = path.basename(inputPath, path.extname(inputPath));
   const pdfPath = path.join(previewsDir, `${baseName}.pdf`);
 
