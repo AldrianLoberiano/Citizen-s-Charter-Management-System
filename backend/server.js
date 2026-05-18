91ef34bdb58a5c2c94dc796b232a6583f0a8dc84
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:08:05 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,6 @@ app.post("/api/uploads/charters", upload.single("file"), (req, res) => {
 });
 
 
-      await convertExcelToPdf(inputPath, previewsDir);
     }
 
     if (!fs.existsSync(pdfPath)) {
