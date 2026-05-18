333308a1923600dfcd97783f66a48633ee126d49
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:08:07 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,6 @@ app.post("/api/uploads/charters", upload.single("file"), (req, res) => {
 });
 
 
-    }
 
     if (!fs.existsSync(pdfPath)) {
       return res.status(500).json({ message: "Failed to generate preview." });
