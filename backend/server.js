b5b690578cfd199955ec3b93e16ffbbc924853c6
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:04:33 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
       return;
-    }
 
     const args = ["--headless", "--norestore", "--convert-to", "pdf", "--outdir", outputDir, inputPath];
     const child = spawn(libreOffice, args, { windowsHide: true });
