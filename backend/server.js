79aa862559f257b7936225cef9fcfb325a356f20
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:06:27 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,6 @@ const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (_req, file, callback) => {
-      "application/vnd.ms-excel",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     ];
     if (!allowed.includes(file.mimetype)) {
