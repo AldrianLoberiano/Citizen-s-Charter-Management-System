6c7adf7179727a37d9255e7c0b0b1a213b3ef32e
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:04:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ fs.mkdirSync(uploadsDir, { recursive: true });
 const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
-const convertExcelToPdf = (inputPath, outputDir) =>
   new Promise((resolve, reject) => {
     const libreOffice = findLibreOffice();
     if (!libreOffice) {
