e4c8bda89af1126d7801d50571e7771afb325400
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:10:26 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,6 @@ app.post("/api/uploads/charters", upload.single("file"), (req, res) => {
 });
 
 
-    }
     res.setHeader("Content-Type", "application/pdf");
     return res.status(200).end();
   } catch (_error) {
