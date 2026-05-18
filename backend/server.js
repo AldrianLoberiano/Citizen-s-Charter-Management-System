ecfeb113b24d0ce9475c8e09f9c51d563eb43247
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:05:04 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ fs.mkdirSync(uploadsDir, { recursive: true });
 const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
-    });
 
     child.on("close", (code) => {
       if (code === 0) {
