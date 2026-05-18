d24977fff654f6f0beea5deca045c44436f536d9
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:03:28 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ fs.mkdirSync(uploadsDir, { recursive: true });
 const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
-  "/snap/bin/libreoffice",
 ].filter(Boolean);
 
 const findLibreOffice = () => libreOfficeCandidates.find((candidate) => fs.existsSync(candidate));
