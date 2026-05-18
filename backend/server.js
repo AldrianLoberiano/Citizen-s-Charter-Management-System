135f96ac17be60b4afeab61f86f2012f462af4b5
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:03:25 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ fs.mkdirSync(uploadsDir, { recursive: true });
 const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
-  "/usr/local/bin/soffice",
   "/snap/bin/libreoffice",
 ].filter(Boolean);
 
