5abab8fceb5b1150cc4a9e2c9bdbaae6c2e72ff6
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:09:29 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,6 @@ app.post("/api/uploads/charters", upload.single("file"), (req, res) => {
 });
 
 
-  if (!fileParam) {
     return res.status(400).end();
   }
   const inputPath = resolveUploadPath(fileParam);
