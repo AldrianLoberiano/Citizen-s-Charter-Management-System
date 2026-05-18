4f4b03c9d1e4b5342ab7837139f3680c875502d6
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 14:04:24 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ fs.mkdirSync(uploadsDir, { recursive: true });
 const backupsDir = path.join(__dirname, "../uploads/backups");
 fs.mkdirSync(backupsDir, { recursive: true });
 
-  new Promise((resolve, reject) => {
     const libreOffice = findLibreOffice();
     if (!libreOffice) {
       reject(new Error("LibreOffice is not installed on the server."));
