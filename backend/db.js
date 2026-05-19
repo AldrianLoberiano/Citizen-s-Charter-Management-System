d8ab9c1a982283ddcd4bf5db126762f7ba5faf99
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 20:54:28 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "root",
