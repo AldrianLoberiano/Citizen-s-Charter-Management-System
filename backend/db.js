8cd1970fa1b96de8e36625c94a52cd3b16b66472
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 20:54:33 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
-  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "ccms_db",
   waitForConnections: true,
