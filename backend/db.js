4725132e8f47d8a3e0616d4291e52faa31b830e2
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 20:54:25 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "root",
