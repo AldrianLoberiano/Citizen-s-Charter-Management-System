08a8dfcd54cdfa1c575b168df6af72aefdad215e
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 21:38:48 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,6 @@ const sqlUpload = multer({
 
 const getDbArgs = () => {
   const dbHost = process.env.DB_HOST || "127.0.0.1";
-  const dbPort = process.env.DB_PORT || "3306";
   const dbUser = process.env.DB_USER || "root";
   const dbPassword = process.env.DB_PASSWORD || "";
   const dbName = process.env.DB_NAME || "ccms_db";
