d9dc03640cafe53eab83b101cac273894e91fa66
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 21:37:27 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,6 @@ const useSsl = String(process.env.DB_SSL || "").toLowerCase() === "true";
 
 export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
-  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "ccms_db",
   waitForConnections: true,
