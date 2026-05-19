1cb8674e6f67f1a5aec85ef34c676ef68758b131
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 20:54:39 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,6 @@ export const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "ccms_db",
-  waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
