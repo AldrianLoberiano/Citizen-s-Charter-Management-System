ce8b62f9c9fd2acd2dbe91a84c434ffe9bf77d82
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 20:55:19 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,7 @@ const getDbArgs = () => {
   const dbPassword = process.env.DB_PASSWORD || "";
   const dbName = process.env.DB_NAME || "ccms_db";
 
+  const baseArgs = ["--host", dbHost, "--port", String(dbPort), "--username", dbUser];
   if (dbPassword) {
     baseArgs.push(`--password=${dbPassword}`);
   }
