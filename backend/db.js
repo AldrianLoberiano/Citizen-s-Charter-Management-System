2ecd4c7fa60af712cd4d4545037611e1bcc58788
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-06 19:06:42 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -11,4 +11,5 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "ccms_db",
+  waitForConnections: true,
 });
