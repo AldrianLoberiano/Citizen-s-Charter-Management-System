10ae991d56ba6b4e1d8eb2c62720593c2eca5dd7
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-06 19:06:21 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 const useSsl = String(process.env.DB_SSL || "").toLowerCase() === "true";
 
-export const pool = new Pool({
   host: process.env.DB_HOST || "127.0.0.1",
   port: Number(process.env.DB_PORT || 5432),
   user: process.env.DB_USER || "postgres",
