d761302ce85bb2ebe2f19b1123999bda29c79020
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-07 11:20:29 +0800

    Update ClientLayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -113,6 +113,7 @@ export function ClientLayout() {
 
       {/* Main Navigation Header */}
       <header
+        className="sticky top-0 z-40 bg-gradient-to-r from-slate-900 via-purple-950 to-slate-900 shadow-none sm:shadow-sm dark:shadow-none"
         style={{
           backgroundImage: `url(${clientHeaderBgSrc})`,
           backgroundSize: "cover",
