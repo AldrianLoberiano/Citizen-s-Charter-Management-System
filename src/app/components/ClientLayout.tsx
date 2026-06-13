efc2fad01fd0c0ab24404174aac2e200584fb45b
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 13:35:14 +0800

    Update ClientLayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -154,6 +154,7 @@ export function ClientLayout() {
           <div className="grid grid-cols-1 items-start gap-8 md:grid-cols-[1.25fr_1fr]">
             {/* About */}
             <div>
+              <div className="mb-3 flex items-center gap-5">
                 <span className="text-slate-900 leading-none dark:text-slate-100">
                   Calauan Citizen's Charter
                 </span>
