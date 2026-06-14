d3f7dd605b2b20afdb1d6a27d804bad700ceafba
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 13:44:03 +0800

    Update AdminLayout.tsx

--- src/app/components/AdminLayout.tsx
+++ src/app/components/AdminLayout.tsx
@@ -299,6 +299,7 @@ export function AdminLayout() {
               In compliance with the <span className="font-medium text-slate-700 dark:text-slate-300">Data Privacy Act of 2012 (RA 10173)</span>.
             </p>
           </div>
+        </footer>
       </div>
 
       <Modal
