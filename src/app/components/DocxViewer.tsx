a57dd9b0f9227797a96d2f044a238a0b18e047c0
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 21:31:16 +0800

    Update DocxViewer.tsx

--- src/app/components/DocxViewer.tsx
+++ src/app/components/DocxViewer.tsx
@@ -60,3 +60,4 @@ export function DocxViewer({ fileUrl, className = "" }: DocxViewerProps) {
     <div
       className={`prose prose-sm max-w-none dark:prose-invert p-6 overflow-auto ${className}`}
       dangerouslySetInnerHTML={{ __html: html }}
+    />
