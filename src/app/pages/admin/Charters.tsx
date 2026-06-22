3381b555362f39b24accbd682e5a65598da556eb
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 21:02:33 +0800

    Update Charters.tsx

--- src/app/pages/admin/Charters.tsx
+++ src/app/pages/admin/Charters.tsx
@@ -596,6 +596,7 @@ export function Charters() {
                   <span className="text-slate-500 text-sm dark:text-slate-400">
                     {uploadingFile ? "Uploading file..." : "Click to upload or drag and drop"}
                   </span>
+                  <span className="text-slate-400 text-xs dark:text-slate-500">PDF or Word Document</span>
                   <input
                     type="file"
                     accept=".pdf"
