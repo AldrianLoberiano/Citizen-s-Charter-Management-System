447683f5370c91aef5307fe0b1d0333a146f2cbb
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 21:01:08 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,7 @@ const upload = multer({
       "application/pdf",
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       "application/msword",
+    ];
     if (!allowed.includes(file.mimetype)) {
       callback(new Error("Only PDF files are allowed."));
       return;
