30d9e30096265a213fa7b534e710645c62ec5d45
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 19:12:16 +0800

    Update ClientLayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -195,6 +195,7 @@ export function ClientLayout() {
                 <li className="flex items-center gap-2">
                   <Facebook className="h-4 w-4 flex-shrink-0 text-slate-400" />
                   <a
+                    href="https://www.facebook.com/p/Business-Permit-and-Licensing-Office-Calauan-Laguna-61576258070290/"
                 </li>
               </ul>
             </div>
