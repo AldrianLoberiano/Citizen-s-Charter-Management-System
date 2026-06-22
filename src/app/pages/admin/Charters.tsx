df5e57767759a3ee7772243a53e09eebf3541652
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 21:02:36 +0800

    Update Charters.tsx

--- src/app/pages/admin/Charters.tsx
+++ src/app/pages/admin/Charters.tsx
@@ -599,7 +599,6 @@ export function Charters() {
                   <span className="text-slate-400 text-xs dark:text-slate-500">PDF or Word Document</span>
                   <input
                     type="file"
-                    accept=".pdf"
                     onChange={handleFileChange}
                     className="hidden"
                     disabled={uploadingFile}
