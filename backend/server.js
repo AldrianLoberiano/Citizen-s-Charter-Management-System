5d0daddbf0db440eec26cdc15fe093733113390b
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-22 21:13:07 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,6 @@ const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (_req, file, callback) => {
-    const allowed = [
       "application/pdf",
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       "application/msword",
