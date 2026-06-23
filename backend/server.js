6f0ebc884677c7f8149fb1af41111feacd7d13d1
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-23 12:07:02 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -627,7 +627,6 @@ app.post("/api/charters/:id/attachment", editedUpload.single("file"), async (req
     return res.status(404).json({ message: "Charter not found" });
   }
 
-  const newRelativePath = `/uploads/charters/${req.file.filename}`;
 
   // Delete old file if it exists
   if (oldFilePath) {
