5e8667b2fb804d586f1ffa38542e93ffebf4d59b
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-26 21:41:08 +0800

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,65 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+
+function figmaAssetResolver() {
+  return {
+    name: 'figma-asset-resolver',
+    resolveId(id) {
+      if (id.startsWith('figma:asset/')) {
+        const filename = id.replace('figma:asset/', '')
+        return path.resolve(__dirname, 'src/assets', filename)
+      }
+    },
+  }
+}
+
+function stripDevCacheValidators() {
+  return {
+    name: 'strip-dev-cache-validators',
+    configureServer(server) {
+      server.middlewares.use((_req, res, next) => {
+        const originalWriteHead = res.writeHead
+        res.writeHead = function patchedWriteHead(...args) {
+          res.removeHeader('ETag')
+          res.removeHeader('Last-Modified')
+          res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+          res.setHeader('Pragma', 'no-cache')
+          res.setHeader('Expires', '0')
+          return originalWriteHead.apply(this, args)
+        }
+        next()
+      })
+    },
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    stripDevCacheValidators(),
+    figmaAssetResolver(),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+  server: {
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+  },
+})
