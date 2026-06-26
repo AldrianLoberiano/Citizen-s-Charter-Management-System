689acf52d21299dcb8265bc0f2366371ac8f0f5f
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-26 23:59:16 +0800

    Update env.d.ts

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,7 +1,30 @@
 /// <reference types="vite/client" />
 
 declare interface ImportMetaEnv {
-  readonly VITE_API_URL?: string;
+  readonly VITE_ENV: "development" | "production";
+  readonly VITE_DEV_BASE_URL: string;
+  readonly VITE_PROD_BASE_URL: string;
+
+  readonly VITE_AUTH_LOGIN_ROUTE: string;
+  readonly VITE_AUTH_LOGOUT_ROUTE: string;
+  readonly VITE_DEPARTMENTS_ROUTE: string;
+  readonly VITE_CHARTERS_ROUTE: string;
+  readonly VITE_EDITED_CHARTERS_ROUTE: string;
+  readonly VITE_RATINGS_ROUTE: string;
+  readonly VITE_FEEDBACK_ROUTE: string;
+  readonly VITE_ADMIN_ROUTE: string;
+  readonly VITE_ADMIN_DASHBOARD_ROUTE: string;
+  readonly VITE_ADMIN_DEPARTMENTS_ROUTE: string;
+  readonly VITE_ADMIN_CHARTERS_ROUTE: string;
+  readonly VITE_ADMIN_EDITED_CHARTERS_ROUTE: string;
+  readonly VITE_ADMIN_FEEDBACK_ROUTE: string;
+  readonly VITE_ADMIN_BACKUP_ROUTE: string;
+  readonly VITE_HOME_ROUTE: string;
+
+  readonly VITE_GSHEETS_API_KEY: string;
+  readonly VITE_GSHEETS_SPREADSHEET_ID: string;
+  readonly VITE_GSHEETS_SHEET_NAME: string;
+  readonly VITE_GSHEETS_RANGE: string;
 }
 
 declare interface ImportMeta {
