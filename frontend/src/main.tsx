decea60710863df797c5f46cd53f0d0a107d0c25
Aldrian Loberiano <loberianorian@gmail.com>
2026-07-05 23:00:48 +0800

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-import { syncLocalCacheFromApi } from "./app/store/apiSync";
+import { syncLocalCacheFromApi, startPolling, stopPolling } from "./app/store/apiSync";
 
 function BootstrapApp() {
     const [ready, setReady] = useState(false);
@@ -11,7 +11,9 @@ function BootstrapApp() {
     useEffect(() => {
       syncLocalCacheFromApi().finally(() => {
         setReady(true);
+        startPolling(5000);
       });
+      return () => stopPolling();
     }, []);
 
     if (!ready) {
@@ -38,3 +40,4 @@ const root = globalScope.__ccmsRoot ?? createRoot(rootElement);
 globalScope.__ccmsRoot = root;
 
 root.render(<BootstrapApp />);
+  
