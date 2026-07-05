6dc7aeb264fa88db0ae153449923c8c35c78547c
Aldrian Loberiano <loberianorian@gmail.com>
2026-07-05 22:00:09 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_POOL_SIZE || 10),
 };
